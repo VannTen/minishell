69ad9355678f7a6c56ecfd2e9fbe03c1a89d2c57
Max Gautier <mgautier@student.42.fr>
2017-04-17 17:02:12 +0200

    Add git ignore + bare execve

--- main.c
+++ main.c
@@ -6,13 +6,48 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 15:48:04 by mgautier          #+#    #+#             */
-/*   Updated: 2017/04/17 15:56:23 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/04/17 16:56:20 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 #include <unistd.h>
+#include <sys/wait.h>
+
+char	*get_raw_input(void)
+{
+	char *input;
+
+	get_next_line(STDIN_FILENO, &input);
+	return (input);
+}
+
+t_input	parse_input(char *raw)
+{
+	char **commands_and_args;
+
+	commands_and_args = ft_strsplit(raw, ' ');
+	ft_strdel(&raw);
+	return (commands_and_args);
+}
+
+int		exec_input(t_input com_and_args)
+{
+	pid_t	child_process;
+	int		stat_loc;
+
+	stat_loc = 0;
+	child_process = fork();
+	if (child_process == 0)
+		execve(com_and_args[0], com_and_args, NULL);
+	else if (child_process > 0)
+	{
+		ft_free_string_array(&com_and_args);
+		wait(&stat_loc);
+	}
+	return (stat_loc);
+}
 
 int main(void)
 {
@@ -25,6 +60,7 @@ int main(void)
 		ft_printf(PROMPT);
 		raw_input = get_raw_input();
 		input = parse_input(raw_input);
-		return_status = exec_input;
+		return_status = exec_input(input);
+		ft_printf("Command return : %d\n", return_status);
 	}
 }
