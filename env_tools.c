dc65190177f2ce3cea14cd99b0543f7179d025ad
Max Gautier <mgautier@student.42.fr>
2017-04-25 17:33:35 +0200

    setenv, unsetenv
    Prototypes, need error checking etc

--- env_tools.c
+++ env_tools.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 17:17:49 by mgautier          #+#    #+#             */
-/*   Updated: 2017/04/25 12:59:32 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/04/25 16:38:17 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ char		*get_key(const char *env_entry)
 
 	index = 0;
 	key = NULL;
-	while (env_entry[index] != '=')
+	while (env_entry[index] != '=' && env_entry != '\0')
 		index++;
 	if (env_entry[index] != '\0')
 		key = ft_strndup(env_entry, index);
@@ -97,12 +97,14 @@ static void			put_entry_value(char *env_entry, const char *new_value)
 {
 	while (*env_entry != '=')
 		env_entry++;
+	env_entry++;
 	while (*new_value != '\0')
 	{
 		*env_entry = *new_value;
 		env_entry++;
 		new_value++;
 	}
+	*env_entry = '\0';
 }
 
 char		*replace_env(char *old_env_entry,
@@ -130,35 +132,104 @@ char		*replace_env(char *old_env_entry,
 	return (old_env_entry);
 }
 
+/*
+char		*add_new_env_entry(const char *key, const char *value,
+size_t size, char **env)
+{
+}
+*/
+
+t_bool		key_are_equal(const char *searched_key, const char *env_entry)
+{
+	size_t	index;
+
+	index = 0;
+	while (searched_key[index] == env_entry[index]
+			&& searched_key[index] != '\0'
+			&& env_entry[index] != '\0')
+		index++;
+	if (env_entry[index] == '=' && searched_key[index] == '\0')
+		return (TRUE);
+	else
+		return (FALSE);
+}
+
+char		**add_new_entry(size_t index, const char *key,
+		const char *value, char **env)
+{
+	char	**new_env;
+
+	new_env = (malloc(sizeof(char*) * (index + 2)));
+	if (new_env != NULL)
+	{
+		copy_env(env, new_env);
+		new_env[index] = ft_strvajoin(3, key, "=", value);
+		new_env[index + 1] = NULL;
+		if (new_env[index] != NULL)
+		{
+			free(env);
+			env = new_env;
+		}
+		else
+		{
+			free(new_env);
+			env = NULL;
+		}
+	}
+	return (env);
+}
 char		**ft_putenv(const char *key, const char *value, char **env)
 {
 	size_t	index;
-	char	**new_env;
+	char	*new_entry;
 
 	index = 0;
-	while (env[index] != NULL && ft_strcmp(key, env[index]) != 0)
+	while (env[index] != NULL && !key_are_equal(key, env[index]))
 		index++;
 	if (env[index] == NULL)
+		env = add_new_entry(index, key, value, env);
+	else
 	{
-		new_env = (malloc(sizeof(char*) * (index + 2)));
-		if (new_env != NULL)
+		new_entry = replace_env(env[index], key, value);
+		if (new_entry != NULL)
+			env[index] = new_entry;
+		else
+			env = NULL;
+	}
+	return (env);
+}
+
+t_bool	is_valid_setenv(const char *arg)
+{
+	size_t	index;
+
+	index = 0;
+	while (arg[index] != '\0' && arg[index] != '=')
+		index++;
+	if (arg[index] == '=')
+		return (TRUE);
+	else
+		return (FALSE);
+}
+
+char	**ft_removeenv(const char *key, char **env)
+{
+	size_t	index;
+
+	index = 0;
+	while (env[index] != NULL)
+	{
+		if (key_are_equal(key, env[index]))
+			break ;
+		index++;
+	}
+	if (env[index] != NULL)
+	{
+		while (env[index] != NULL)
 		{
-			copy_env(env, new_env);
-			new_env[index] = ft_strvajoin(3, key, "=", value);
-			new_env[index + 1] = NULL;
-			if (new_env[index] != NULL)
-			{
-				free(env);
-				env = new_env;
-			}
-			else
-			{
-				free(new_env);
-				env = NULL;
-			}
+			env[index] = env[index + 1];
+			index++;
 		}
 	}
-	else
-		env[index] = replace_env(env[index], key, value);
 	return (env);
 }
--- includes/env_interface.h
+++ includes/env_interface.h
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/18 17:30:14 by mgautier          #+#    #+#             */
-/*   Updated: 2017/04/24 20:09:26 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/04/25 16:38:37 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ const char	*get_env_value(const char *key);
 char		*get_env_entry(const char *key);
 char		*get_key(const char *env_entry);
 char		**get_path(void);
+t_bool		is_valid_setenv(const char *arg);
 
 char		**ft_putenv(const char *key, const char *value, char **env);
+char		**ft_removeenv(const char *key, char **env);
 const char	*get_value(const char *env_entry);
 
 #endif
