45f6bbf1dfad4794dd158d4c691110a298c85bce
Max Gautier <mgautier@student.42.fr>
2017-05-02 10:15:32 +0200

    Add error codes defines

---
+++ includes/error_interface.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_interface.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/19 17:12:41 by mgautier          #+#    #+#             */
+/*   Updated: 2017/04/19 17:13:26 by mgautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_INTERFACE_H
+# define ERROR_INTERFACE_H
+
+enum	e_error
+{
+	SHELL_ENOMEM,
+	ERROR_CODE_NBR
+};
+
+#endif
