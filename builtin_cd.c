e06eef34906430d4e8374a3feb4265cf4037e6a1
Max Gautier <mgautier@student.42.fr>
2017-05-05 17:18:11 +0200

    Cd posix

---
+++ builtin_cd.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_cd.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/05 17:13:46 by mgautier          #+#    #+#             */
+/*   Updated: 2017/05/05 17:17:55 by mgautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Attempt to implement cd following the steps prescribed by POSIX at
+** http://pubs.opengroup.org/onlinepubs/9699919799/utilities/cd.html
+*/
+
+static
+int	ft_cd(const char **argv, t_shell *shell_state)
+{
+
+}
---
+++ includes/builtin_cd_defs.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_cd_defs.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/05 17:15:30 by mgautier          #+#    #+#             */
+/*   Updated: 2017/05/05 17:16:40 by mgautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_CD_DEFS_H
+# define BUILTIN_CD_DEFS_H
+
+typedef struct s_cd_param	t_cd_param;
+
+struct s_cd_param
+{
+
+};
+
+#endif
