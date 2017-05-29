a5d0875978caa2b4c1723850b564e8b473aac45e
Max Gautier <mgautier@student.42.fr>
2017-05-29 13:41:03 +0200

    Move exit in its own file

--- builtins.c
+++ builtins.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 18:24:32 by mgautier          #+#    #+#             */
-/*   Updated: 2017/05/23 17:41:56 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/05/29 11:50:31 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,31 +16,13 @@
 #include "libft.h"
 #include "error_interface.h"
 #include <stdlib.h>
-#include <sys/param.h>
 
 int			ft_echo(const char **argv, t_shell *shell_state)
 {
 	(void)shell_state;
 	ft_print_string_array(argv + 1, ' ');
 	ft_putchar('\n');
-	return (EXIT_SUCCESS);
-}
-
-int			ft_exit(const char **argv, t_shell *shell_state)
-{
-	int	exit_status;
-
-	if (argv[1] != NULL)
-	{
-		exit_status = string_is_digit(argv[1]) ?
-			ft_strict_atoi(argv[1]) :
-			EXIT_STATUS_OUT_OF_RANGE;
-	}
-	else
-		exit_status = EXIT_SUCCESS;
-	set_exit_status(shell_state, exit_status);
-	will_exit(shell_state);
-	return (exit_status);
+	return (BUILTIN_EXIT_SUCCESS);
 }
 
 int			ft_setenv(const char **argv, t_shell *shell_state)
