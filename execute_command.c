35c57bea5e8aacad49b9d56b4f6677e4a4b956c7
Max Gautier <mgautier@student.42.fr>
2017-05-30 12:01:05 +0200

    Add signal information

--- execute_command.c
+++ execute_command.c
@@ -6,17 +6,31 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/29 19:03:17 by mgautier          #+#    #+#             */
-/*   Updated: 2017/05/29 19:08:30 by mgautier         ###   ########.fr       */
+/*   Updated: 2017/05/30 12:00:53 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "error_interface.h"
+#include "libft.h"
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int		execute_command(char *const *args, char **env)
+static void	signal_error(int signal_number)
+{
+	const char	*signal_str[] = {
+		"HUP", "INT", "QUIT", "ILL", "TRAP", "TABRT", "EMT", "FPE", "KILL",
+		"BUS", "SEGV", "SYS", "PIPE", "ALRM", "TERM", "URG", "STOP", "STP",
+		"CONT", "CHLD", "TTIN", "TTOU", "IO", "XCPU", "VTALRM", "PROF",
+		"WINCH", "INFO", "USR1", "USR2"};
+
+	if (signal_number >= 1 && signal_number <= 31)
+		ft_dprintf(STDERR_FILENO, "Process received signal SIG%s\n",
+				signal_str[signal_number - 1]);
+}
+
+int			execute_command(char *const *args, char **env)
 {
 	pid_t	child;
 	int		status;
@@ -35,7 +49,10 @@ int		execute_command(char *const *args, char **env)
 	if (WIFEXITED(status))
 		status = WEXITSTATUS(status);
 	else if (WIFSIGNALED(status))
+	{
 		status = 128 + WTERMSIG(status);
+		signal_error(WTERMSIG(status));
+	}
 	else if (WIFSTOPPED(status))
 		status = WSTOPSIG(status);
 	return (status);
